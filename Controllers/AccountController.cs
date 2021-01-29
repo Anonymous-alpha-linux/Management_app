5e37a43c480d352632997e98a137c4049f0dd32b
tinhdine5 <trungtinh246810f@gmail.com>
2021-01-29 21:31:16 +0700

    Fix return pageview after adding new user

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -165,15 +165,14 @@ namespace Management_app.Controllers
                 if (result.Succeeded)
                 {
                     result = await UserManager.AddToRoleAsync(user.Id, model.Role);
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
+                    
+                    return user.Roles.Single().ToString() == "staff"?RedirectToAction("StaffAccountView","Admin") : RedirectToAction("TrainerAccountView","Admin");
                 }
                 var list = new List<string>() { "admin", "staff", "trainer", "trainee" };
                 ViewBag.list = list;
